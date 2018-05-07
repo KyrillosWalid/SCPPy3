7e304778b8718cfd05ac06b6ec4d5a6ece589d58
Kyrillos Walid Radi <30301905+KOKADev@users.noreply.github.com>
2018-05-07 04:03:09 +0200

    Create S4AP.py

---
+++ A. Watermelon/S4AP.py
@@ -0,0 +1,18 @@
+w = int(input(""))
+if (w == 2):
+    print("NO")
+else:
+    if (w % 2 == 0):
+        print("YES")
+    else:
+        print("NO")
+'''
+**********************************************************
+* THIS CODE BY KOKADEV...KYRILLOSWALIDRADI               *
+* ALL COPYRIGHYS ARE SAVED TO KYRILLOSWALIDRADI          *
+* + 2 0 1 0 6 2 7 0 4 4 1 3 - + 2 0 1 2 1 1 2 8 0 3 5 8  *
+* KYRILLOSWALIDRADI@HOTMAIL.COM                          *
+* WWW.FACEBOOK.COM/KOKADEV                               *
+* WWW.GITHUB.COM/KOKADev                                 *
+**********************************************************
+'''
